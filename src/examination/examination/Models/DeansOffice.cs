156841dc8050c30e461e443e896c61668a4f0acd
ivanbiryukovv <ivanbiryukov2021@gmail.com>
2026-07-02 09:32:11 +0300

    Add files via upload
    Описаны классы Student и DeansOffice

---
+++ src/examination/examination/Models/DeansOffice.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace examination.Models
+{
+    class DeansOffice
+    {
+        public List<Student> Students;
+        public void sortStudents(List<Student> students)
+        {
+            var size = students.Count;
+            for (int i = 0; i < size - 1; i++)
+            {
+                for (int j = 0; j < size - 1; j++)
+                {
+                    if (string.Compare(Students[j].surname, Students[j + 1].surname) > 0 
+                        && string.Compare(Students[j].name, Students[j + 1].name) > 0)
+                    {
+                        var temp = students[j];
+                        students[j] = students[j + 1];
+                        students[j + 1] = temp;
+                    }
+                }
+            }
+        }
+
+        public void saveToFile(List<Student> students)
+        {
+            string filePath = "data.txt";
+            using (StreamWriter sw = new StreamWriter(filePath))
+            {
+                for (int i = 0; i < students.Count; i++)
+                {
+                    sw.WriteLine($"{students[i].surname}, {students[i].name}, {students[i].number}");
+                }
+            }
+        }
+    }
+}
---
+++ src/examination/examination/Models/Student.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace examination.Models
+{
+    class Student
+    {
+        public string surname { get; set; }
+        public string name { get; set; }
+        public int number { get; set; }
+    }
+}
